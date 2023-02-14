5f7fe7d68caadc76624158abad6f0885623990e2
kkm5291 <kkm23125291@gmail.com>
2023-02-14 15:17:07 +0900

    Docs : 진행상황 수정

--- Baekjoon/B_11399.py
+++ Baekjoon/B_11399.py
@@ -4,11 +4,12 @@ n = int(sys.stdin.readline())
 
 time = list(map(int, sys.stdin.readline().rstrip().split()))
 
-time.sort()
+time.sort() # O(n log n)
 
 result = 0
 acc_result = 0
 
+# O(n)
 for i in time:
     acc_result += i
     result += acc_result
@@ -23,3 +24,5 @@ print(result)
 
 # 슬라이싱을 이용한 풀이방법과 누적합으로 구하는 방법으로 진행함
 # 슬라이싱을 이용한 방법이 8ms 더 느렸음. 슬라이싱은 index로 list를 돌아다녀야 해서 느려지는 것인가??????????????
+
+# 그러면 O(nlogn)이랑 O(n) 같이 있으니까 더 큰 O(nlogn)이 복잡도인가?
