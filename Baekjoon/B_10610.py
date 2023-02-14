c8b9bde510d5b3bafb29302a30648478b41097ae
kkm5291 <kkm23125291@gmail.com>
2023-02-14 19:20:17 +0900

    10610 30 : 다른사람 풀이 추가

---
+++ Baekjoon/B_10610.py
@@ -0,0 +1,35 @@
+# 내풀이
+# 예제에서 가장 큰수로 나열하길래 그렇게 풀었음;;;;;;
+# 이게 도대체 왜 그리디인가 생각해보면서 풀었음;;;;;
+
+# import sys
+
+# n = list(sys.stdin.readline().rstrip())
+
+# n.sort(reverse=True)
+
+# number = int(''.join(n))
+    
+# if number % 30 == 0:
+#     print(number)
+# else:
+#     print(-1)
+
+# 다른사람 풀이
+# 해당 수가 3의 배수임을 알기 위한 공식이 필요함
+# 해당 수가 3의 배수임을 알기위해서는 각 자리의 수의 합이 3이 되어야 함
+
+import sys
+
+n = list(sys.stdin.readline().rstrip())
+
+n.sort(reverse=True)
+
+sum=0
+for i in n:
+    sum += int(i)
+
+if sum % 3 != 0 or "0" not in n:
+    print(-1)
+else:
+    print(''.join(n))
