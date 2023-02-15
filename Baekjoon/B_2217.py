19a0e63585ebe01cfa7c222493b46a23199c0b82
kkm5291 <kkm23125291@gmail.com>
2023-02-15 12:34:09 +0900

    2217 로프

---
+++ Baekjoon/B_2217.py
@@ -0,0 +1,18 @@
+import sys
+
+# k개의 로프를 활용하여 중량이 w인 물체를 들어올릴 때, 각각의 로프에는 모두 w/k만큼의 중량이다.
+
+# 로프들을 이용해 들 수 있는 최대 중량 구하기
+
+n = int(sys.stdin.readline())
+rope = []
+for _ in range(n):
+    rope.append(int(sys.stdin.readline().rstrip()))
+
+rope.sort(reverse=True)
+result = []
+
+for i in range(n):
+    result.append(rope[i] * (i+1))
+
+print(max(result))
