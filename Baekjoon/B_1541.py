2f5fa9e9d7a98a40537457fb4953e252d3a8ea72
kkm5291 <kkm23125291@gmail.com>
2023-02-15 12:33:52 +0900

    1541 잃어버린괄호

---
+++ Baekjoon/B_1541.py
@@ -0,0 +1,17 @@
+import sys
+
+arr = sys.stdin.readline().split('-')
+num = []
+for i in arr:
+    sum = 0
+    s = i.split('+')
+    for j in s:
+        sum += int(j)
+    
+    num.append(sum)
+
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+
+print(n)
