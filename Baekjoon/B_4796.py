f2ace8241a67287c577c4ccbcc761568637b083f
kkm5291 <kkm23125291@gmail.com>
2023-02-15 15:16:50 +0900

    4796 캠핑 : 반례 찾지 못해서 헤맸음

---
+++ Baekjoon/B_4796.py
@@ -0,0 +1,12 @@
+import sys
+
+result = 0
+i=1
+while True:
+    l, p, v = map(int, sys.stdin.readline().split())
+    if l+p+v == 0:
+        break
+    result = (v // p) * l
+    result += min((v%p), l)
+    print("Case " + str(i) + ": " + str(result))
+    i+=1
