cfe1790d98d6ea7900f738ca7f1cf6e576b6ac1a
kkm5291 <kkm23125291@gmail.com>
2023-02-16 14:24:56 +0900

    1946 신입사원 - 문제의 핵심은 모든 지원자들을 각자 분석하는 것이 아니였음 - 바로 뒷 순위의 지원자와 비교해서 점수가 높으면 합격 - 즉, man.sort()로 서류심사 성적으로 분류한 후 - 면접점수만 뒷 지원자와 비교하면 되는 문제 * 초반 t는 20까지의 수였기 때문에 첫 for문 안에서 for문 하나는 시간복잡도상 허용이 가능한 수치였음 * 다만, 이중포문일 경우 O(n^2)의 시간복잡도가 나오므로 최대 n값인 100_000만을 커버할 수 없게 됨

---
+++ Baekjoon/B_1946.py
@@ -0,0 +1,42 @@
+import sys
+
+# 우선 입력값이 최대 100_000 이기 때문에 O(n^2) 을 넘어가면 안되기 때문에 그리디
+# 모두 점수가 높은 애를 전체에서 뺀다?
+
+t = int(sys.stdin.readline())
+
+# man2= [[3,6],[7,3],[4,2],[1,4],[5,7],[2,5],[6,1]]
+
+# manSum = [5,5,5,5,10]
+# manSum.sort()
+
+
+for i in range(t):
+    man = []
+    n = int(sys.stdin.readline())
+    result = 1
+    for j in range(n):
+        score = list(map(int, sys.stdin.readline().split()))
+        man.append(score)
+    
+    
+    man.sort(key=lambda x: x[0])
+
+    target = man[0][1]
+    for k in range(1, n):
+        if target > man[k][1]:
+            result += 1
+            target = man[k][1]
+
+
+    print(result)
+    
+    
+
+# man.sort(key=lambda x:(x[0], x[1]))
+# [[1, 4], [2, 3], [3, 2], [4, 1], [5, 5]]
+
+# man2.sort(reverse=True, key=lambda x:(x[0], x[1]))
+# [[7, 3], [6, 1], [5, 7], [4, 2], [3, 6], [2, 5], [1, 4]]
+# 10        7       12      6       9       7       5
+
