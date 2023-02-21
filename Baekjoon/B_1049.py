e08252b26f65bb03f671a26c80a47de13619adbf
kkm5291 <kkm23125291@gmail.com>
2023-02-21 15:41:10 +0900

    1049 다른사람풀이 추가 (분기문으로만 접근)

--- Baekjoon/B_1049.py
+++ Baekjoon/B_1049.py
@@ -1,29 +1,50 @@
 import sys
-input = sys.stdin.readline
+# input = sys.stdin.readline
 
 
-guitar, brand = map(int,input().split())
+# guitar, brand = map(int,input().split())
 
 
-pakageBrands = [list(map(int, input().split())) for _ in range(brand)]
-singleBrands = pakageBrands.copy()
+# pakageBrands = [list(map(int, input().split())) for _ in range(brand)]
+# singleBrands = pakageBrands.copy()
 
-pakageBrands.sort(key=lambda x:(x[0]))
-singleBrands.sort(key=lambda x:x[1])
+# pakageBrands.sort(key=lambda x:(x[0]))
+# singleBrands.sort(key=lambda x:x[1])
 
-pakageMinCost = pakageBrands[0][0]
-singleMinCost = singleBrands[0][1]
+# pakageMinCost = pakageBrands[0][0]
+# singleMinCost = singleBrands[0][1]
 
-result = 0
+# result = 0
 
-while True:
-    temp = guitar // 6
-    if guitar <= 6:
-        minCost = min(pakageMinCost, singleMinCost * guitar)
-        result += minCost
-        break
-    minCost = min(pakageMinCost, singleMinCost * 6)
-    result += temp * minCost
-    guitar -= temp * 6
+# while True:
+#     temp = guitar // 6
+#     if guitar <= 6:
+#         minCost = min(pakageMinCost, singleMinCost * guitar)
+#         result += minCost
+#         break
+#     minCost = min(pakageMinCost, singleMinCost * 6)
+#     result += temp * minCost
+#     guitar -= temp * 6
     
-print(result)
+# print(result)
+
+n,m = map(int, input().split())
+
+pakage = []
+single = []
+
+for i in range(m):
+    a,b = input().split()
+    pakage.append(int(a))
+    single.append(int(b))
+
+pMin = min(pakage)
+sMin = min(single)
+
+if pMin < sMin * 6:
+    if pMin < (n%6)*sMin:
+        print((n//6) * pMin + pMin)
+    else:
+        print((n//6) * pMin + (n%6) * sMin)
+else:
+    print(sMin * n)
