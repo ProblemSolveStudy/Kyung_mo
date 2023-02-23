da2c74672edcfec179518631cf52fff08d52f5f8
kkm5291 <kkm23125291@gmail.com>
2023-02-23 15:25:28 +0900

    1543 문서 : 문제 이해를 잘못해서 답지봄;;;; 그래도 다시풀어볼것

---
+++ Baekjoon/B_1543.py
@@ -0,0 +1,15 @@
+import sys
+
+doc = sys.stdin.readline().rstrip()
+word = sys.stdin.readline().rstrip()
+
+cnt = 0
+i = 0
+while i <= len(doc) - len(word):
+    if doc[i:i+len(word)] == word:
+        cnt+=1
+        i += len(word)
+    else:
+        i += 1
+
+print(cnt)
