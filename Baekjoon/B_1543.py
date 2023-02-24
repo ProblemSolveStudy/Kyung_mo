0797f578407ce768b2d4820f8433bc9ca0b9bee8
kkm5291 <kkm23125291@gmail.com>
2023-02-24 09:18:36 +0900

    1543 문서 검색 : 문제 복기

--- Baekjoon/B_1543.py
+++ Baekjoon/B_1543.py
@@ -1,15 +1,30 @@
+# import sys
+
+# doc = sys.stdin.readline().rstrip()
+# word = sys.stdin.readline().rstrip()
+
+# cnt = 0
+# i = 0
+# while i <= len(doc) - len(word):
+#     if doc[i:i+len(word)] == word:
+#         cnt+=1
+#         i += len(word)
+#     else:
+#         i += 1
+
+# print(cnt)
+
 import sys
 
-doc = sys.stdin.readline().rstrip()
+docs = sys.stdin.readline().rstrip()
 word = sys.stdin.readline().rstrip()
 
 cnt = 0
 i = 0
-while i <= len(doc) - len(word):
-    if doc[i:i+len(word)] == word:
-        cnt+=1
+while i<= len(docs) - len(word):
+    if docs[i:i+len(word)] == word:
+        cnt += 1
         i += len(word)
     else:
         i += 1
-
 print(cnt)
