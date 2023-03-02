ac68cd1ff6a153337b45173586d11405a51e448a
kkm5291 <kkm23125291@gmail.com>
2023-03-02 09:50:01 +0900

    1260 DFS와 BFS : 안외워져;;;

--- study/B_1260.py
+++ study/B_1260.py
@@ -1,19 +1,45 @@
+# import sys
+# from collections import deque
+
+# n,m,v = map(int, sys.stdin.readline().split())
+
+# graph = [[] for _ in range(n+1)]
+
+# for _ in range(m):
+#     a, b = map(int, sys.stdin.readline().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+
+# for i in graph:
+#     i.sort()
+
+# visited = [False] * (n+1)
+
+# def dfs(graph, v, visited):
+#     visited[v] = True
+#     print(v, end=' ')
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(graph, i, visited)
+
+# def bfs(graph, start, visited):
+#     queue = deque([start])
+#     visited[start] = True
+#     while queue:
+#         v = queue.popleft()
+#         print(v, end=' ')
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i] = True
+ 
+# dfs(graph, v, visited)
+# visited = [False] * (n+1)
+# print()
+# bfs(graph, v, visited)
+
 import sys
 from collections import deque
-
-n,m,v = map(int, sys.stdin.readline().split())
-
-graph = [[] for _ in range(n+1)]
-
-for _ in range(m):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-for i in graph:
-    i.sort()
-
-visited = [False] * (n+1)
 
 def dfs(graph, v, visited):
     visited[v] = True
@@ -32,8 +58,22 @@ def bfs(graph, start, visited):
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
- 
-dfs(graph, v, visited)
+
+n,m,v = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(n+1)]
+
 visited = [False] * (n+1)
+
+for _ in range(m):
+    a,b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in graph:
+    i.sort()
+
+dfs(graph, v, visited)
 print()
+visited = [False] * (n+1)
 bfs(graph, v, visited)
