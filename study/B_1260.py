5b70826423c7d896eb32637af0eb8008ce95142b
kkm5291 <kkm23125291@gmail.com>
2023-03-03 15:00:56 +0900

    1260 DFS와 BFS : 문제 재풀이

--- study/B_1260.py
+++ study/B_1260.py
@@ -1,45 +1,5 @@
-# import sys
-# from collections import deque
-
-# n,m,v = map(int, sys.stdin.readline().split())
-
-# graph = [[] for _ in range(n+1)]
-
-# for _ in range(m):
-#     a, b = map(int, sys.stdin.readline().split())
-#     graph[a].append(b)
-#     graph[b].append(a)
-
-# for i in graph:
-#     i.sort()
-
-# visited = [False] * (n+1)
-
-# def dfs(graph, v, visited):
-#     visited[v] = True
-#     print(v, end=' ')
-#     for i in graph[v]:
-#         if not visited[i]:
-#             dfs(graph, i, visited)
-
-# def bfs(graph, start, visited):
-#     queue = deque([start])
-#     visited[start] = True
-#     while queue:
-#         v = queue.popleft()
-#         print(v, end=' ')
-#         for i in graph[v]:
-#             if not visited[i]:
-#                 queue.append(i)
-#                 visited[i] = True
- 
-# dfs(graph, v, visited)
-# visited = [False] * (n+1)
-# print()
-# bfs(graph, v, visited)
-
-import sys
 from collections import deque
+import sys
 
 def dfs(graph, v, visited):
     visited[v] = True
@@ -59,21 +19,22 @@ def bfs(graph, start, visited):
                 queue.append(i)
                 visited[i] = True
 
-n,m,v = map(int, sys.stdin.readline().split())
+
+input = sys.stdin.readline
+n,m,v = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
-
 visited = [False] * (n+1)
 
-for _ in range(m):
-    a,b = map(int, sys.stdin.readline().split())
+for i in range(m):
+    a,b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-for i in graph:
-    i.sort()
+for i in range(len(graph)):
+    graph[i].sort()
 
 dfs(graph, v, visited)
-print()
 visited = [False] * (n+1)
+print()
 bfs(graph, v, visited)
