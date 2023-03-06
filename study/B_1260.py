20b9b08e0845b00db3250db8a8a11ead252feaf5
kkm5291 <kkm23125291@gmail.com>
2023-03-06 16:28:54 +0900

    1260 DFS와 BFS : 문제 재풀이

--- study/B_1260.py
+++ study/B_1260.py
@@ -1,16 +1,9 @@
-from collections import deque
 import sys
+from collections import deque
 
-def dfs(graph, v, visited):
+def bfs(graph, v, visited):
+    queue = deque([v])
     visited[v] = True
-    print(v, end=' ')
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-
-def bfs(graph, start, visited):
-    queue = deque([start])
-    visited[start] = True
     while queue:
         v = queue.popleft()
         print(v, end=' ')
@@ -19,15 +12,20 @@ def bfs(graph, start, visited):
                 queue.append(i)
                 visited[i] = True
 
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-input = sys.stdin.readline
-n,m,v = map(int, input().split())
+n,m,v = map(int,sys.stdin.readline().split())
 
 graph = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
 
-for i in range(m):
-    a,b = map(int, input().split())
+for _ in range(m):
+    a,b = map(int, sys.stdin.readline().rstrip().split())
     graph[a].append(b)
     graph[b].append(a)
 
@@ -35,6 +33,6 @@ for i in range(len(graph)):
     graph[i].sort()
 
 dfs(graph, v, visited)
-visited = [False] * (n+1)
 print()
+visited = [False] * (n+1)
 bfs(graph, v, visited)
