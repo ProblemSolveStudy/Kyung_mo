e17fa495e33cef832b660d7d7217f2f97d6fc6ed
kkm5291 <kkm23125291@gmail.com>
2023-03-06 16:29:16 +0900

    2178 미로탐색 : 문제 재풀이이

--- study/B_2178.py
+++ study/B_2178.py
@@ -1,11 +1,11 @@
 from collections import deque
 import sys
+
 def bfs(x,y):
     queue = deque()
     queue.append((x,y))
     while queue:
         x,y = queue.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -18,14 +18,11 @@ def bfs(x,y):
                 graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
     return graph[n-1][m-1]
-        
 
-input = sys.stdin.readline
 
-n,m = map(int, input().split())
-graph = [list(map(int, input().rstrip())) for _ in range(n)]
+n,m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(n)]
 
-# 상 하 좌 우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
