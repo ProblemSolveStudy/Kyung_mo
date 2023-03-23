0eb1a2bbbb56e2395149de20b93f6717ddd6334c
kkm5291 <kkm23125291@gmail.com>
2023-03-23 09:50:04 +0900

    18870 좌표 압축 : 왜 자꾸 dictionary를 까먹니;;;

--- Baekjoon/B_18870.py
+++ Baekjoon/B_18870.py
@@ -1,11 +1,12 @@
 import sys
-import copy
+
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().rstrip().split()))
-arr_copy = copy.deepcopy(arr)
-
-arr_copy = list(set(arr_copy))
-arr_copy.sort()
+a = list(set(arr))
+a.sort()
+dic = {}
+for i in range(len(a)):
+    dic[a[i]] = i
 
 for i in arr:
-    print(arr_copy.index(i), end=' ')
+    print(dic[i], end=' ')
