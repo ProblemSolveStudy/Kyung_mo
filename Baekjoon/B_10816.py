bcf4d3aac552bbbdca4f110463686c01d4015587
kkm5291 <kkm23125291@gmail.com>
2023-03-24 10:21:39 +0900

    10816 숫자카드2 : 이분탐색 다른풀이

--- Baekjoon/B_10816.py
+++ Baekjoon/B_10816.py
@@ -1,24 +1,28 @@
 import sys
+input = sys.stdin.readline
 
-_ = int(sys.stdin.readline())
-n_arr = list(map(int, sys.stdin.readline().rstrip().split()))
-n_arr.sort()
-_ = int(sys.stdin.readline())
-m_arr = list(map(int, sys.stdin.readline().rstrip().split()))
+N = int(input())
+cards = sorted(list(map(int, input().split())))
+M = int(input())
+candidate = list(map(int, input().split()))
 
-def binary_search(array, target, start, end):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return array[start:end+1].count(array[mid])
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid-1)
+cnt = {}
+for card in cards:
+    if card in cnt:
+        cnt[card] += 1
     else:
-        return binary_search(array, target, mid+1, end)
+        cnt[card] = 1
 
-dic = {}
-for i in n_arr:
-    if i not in dic:
-        dic[i] = binary_search(n_arr, i, 0, len(n_arr)-1)
-print(' '.join(str(dic[x]) if x in dic else '0' for x in m_arr))
+def binary_search(arr, target, start, end):
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+    if arr[mid] == target:
+        return cnt.get(target)
+    elif arr[mid] > target:
+        return binary_search(arr, target, start, mid-1)
+    else:
+        return binary_search(arr, target, mid+1, end)
+
+for target in candidate:
+    print(binary_search(cards, target, 0, N-1), end=' ')
