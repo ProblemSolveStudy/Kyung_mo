84960b60fbc7e0fea23bf6192a06f02ec94387ee
kkm5291 <kkm23125291@gmail.com>
2023-03-27 09:28:46 +0900

    10816 숫자 카드2

--- Baekjoon/B_10816.py
+++ Baekjoon/B_10816.py
@@ -1,13 +1,14 @@
+# 일단 주어지는 입력의 수가 1천만이다.
+# 게다가 시간제한은 1초기 때문에 O(n)이하의 시간복잡도로 문제를 풀어야 한다.
 import sys
-input = sys.stdin.readline
 
-N = int(input())
-cards = sorted(list(map(int, input().split())))
-M = int(input())
-candidate = list(map(int, input().split()))
+N = int(sys.stdin.readline())
+n_arr = sorted(list(map(int, sys.stdin.readline().split())))
+M = int(sys.stdin.readline())
+m_arr = list(map(int, sys.stdin.readline().split()))
 
 cnt = {}
-for card in cards:
+for card in n_arr:
     if card in cnt:
         cnt[card] += 1
     else:
@@ -19,10 +20,10 @@ def binary_search(arr, target, start, end):
     mid = (start + end) // 2
     if arr[mid] == target:
         return cnt.get(target)
-    elif arr[mid] > target:
+    if arr[mid] > target:
         return binary_search(arr, target, start, mid-1)
     else:
         return binary_search(arr, target, mid+1, end)
 
-for target in candidate:
-    print(binary_search(cards, target, 0, N-1), end=' ')
+for target in m_arr:
+    print(binary_search(n_arr, target, 0, N-1), end=' ')
