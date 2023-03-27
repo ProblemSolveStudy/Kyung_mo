dafc953e4c712d0c56333256971e5e6c306fa8af
kkm5291 <kkm23125291@gmail.com>
2023-03-27 12:38:05 +0900

    1300 K번째 수 : 문제 재풀이

--- Baekjoon/B_1300.py
+++ Baekjoon/B_1300.py
@@ -5,6 +5,7 @@ K = int(sys.stdin.readline())
 start = 1
 end = N*N
 
+result = 0
 while start <= end:
     mid = (start + end) // 2
     total = 0
@@ -12,8 +13,9 @@ while start <= end:
         total += min(mid//i, N)
 
     if total >= K:
+        result = mid
         end = mid - 1
     else:
         start = mid + 1
 
-print(start)
+print(result)
