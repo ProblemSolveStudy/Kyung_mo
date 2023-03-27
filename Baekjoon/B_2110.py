174969e11739169d7389d394336f8348cf1b41df
kkm5291 <kkm23125291@gmail.com>
2023-03-27 10:47:37 +0900

    2110 공유기 설치 : 주석 설명 추가

--- Baekjoon/B_2110.py
+++ Baekjoon/B_2110.py
@@ -5,23 +5,23 @@ n,c = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 arr.sort()
 
-start = 0
-end = max(arr)
+start = 0 # 집의 좌표는 0부터 시작한다
+end = max(arr) # 집의 좌표중 가장 마지막 숫자
 
-result = 0
-while (start<= end):
-    total = 1
-    mid = (start+end)//2
+result = 0 # 값을 갱신해줄 변수
+while (start<= end): # 최적의 해를 찾기 위한 조건
     current = arr[0]
+    total = 1 # arr[0]을 잡고 시작한다는 마인드
+    mid = (start+end)//2 # 중간값 잡아주고
 
-    for i in range(1, len(arr)):
-        if arr[i] >= current + mid:
+    for i in range(1, len(arr)): # current 다음수부터 하나씩 검사한다
+        if arr[i] >= current + mid: # current에서 mid값을 더한 값보다 크거나 같으면 total 증가
             total += 1
-            current = arr[i]
+            current = arr[i] # current값 갱신해주고
     
-    if total < c:
+    if total < c: # c보다 작으니 mid값을 줄여서 더 짧은범위를 탐색하게 할 것
         end = mid - 1
-    else:
+    else: # c보다 많이 찾았으니 result에 우선 mid값 넣고, 최적의 해를 찾기 위해서 start값 갱신
         result = mid
         start = mid + 1
 
