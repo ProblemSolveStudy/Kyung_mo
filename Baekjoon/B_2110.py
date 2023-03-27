6c6d1e7c558f72e8467492aecf9de39261779c5e
kkm5291 <kkm23125291@gmail.com>
2023-03-27 10:37:39 +0900

    2110 공유기 설치 : 개념정립 다시할 것

--- Baekjoon/B_2110.py
+++ Baekjoon/B_2110.py
@@ -1,6 +1,28 @@
 import sys
-from bisect import bisect_left, bisect_right
+input = sys.stdin.readline
 
-n,c = map(int, sys.stdin.readline().split())
-arr = [int(sys.stdin.readline()) for _ in range(n)]
+n,c = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+arr.sort()
 
+start = 0
+end = max(arr)
+
+result = 0
+while (start<= end):
+    total = 1
+    mid = (start+end)//2
+    current = arr[0]
+
+    for i in range(1, len(arr)):
+        if arr[i] >= current + mid:
+            total += 1
+            current = arr[i]
+    
+    if total < c:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
