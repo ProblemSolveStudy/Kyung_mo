d1b331d4ac82d7669a2d0526fada32a714e476b3
kkm5291 <kkm23125291@gmail.com>
2023-03-30 10:40:05 +0900

    1300 K번째 수

--- Baekjoon/B_1300.py
+++ Baekjoon/B_1300.py
@@ -1,21 +1,19 @@
 import sys
-N = int(sys.stdin.readline())
-K = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
 
 start = 1
-end = N*N
-
+end = n*n
 result = 0
 while start <= end:
     mid = (start + end) // 2
     total = 0
-    for i in range(1, N+1):
-        total += min(mid//i, N)
-
-    if total >= K:
-        result = mid
-        end = mid - 1
-    else:
+    for i in range(1, n+1):
+        total += min(mid // i, n)
+    
+    if total < k: 
         start = mid + 1
-
+    else:
+        result = mid # 최소 mid 값을 찾기
+        end = mid - 1
 print(result)
