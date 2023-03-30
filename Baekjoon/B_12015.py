7cd6586f411a6a22e4b966c6b70cf6c33bc8eb1f
kkm5291 <kkm23125291@gmail.com>
2023-03-30 12:21:01 +0900

    12015 가장 긴 증가하는 부분 수열 : 어려운데?

--- Baekjoon/B_12015.py
+++ Baekjoon/B_12015.py
@@ -1,22 +1,40 @@
+# import sys
+# from bisect import bisect_left, bisect_right
+# input = sys.stdin.readline
+
+# n = int(input())
+# arr = list(map(int, input().split()))
+# dp = [0]
+
+# for a in arr:
+#     if dp[-1] < a:
+#         dp.append(a)
+#     else:
+#         dp[bisect_left(dp,a)] = a
+
+# print(len(dp) - 1)
+
+
 import sys
-from bisect import bisect_left, bisect_right
 input = sys.stdin.readline
 
 n = int(input())
+cases = list(map(int, input().split()))
+lis = [0]
 
-# [10, 10, 20, 20, 30, 50]
-arr = list(map(int, input().rstrip().split()))
-arr.sort()
-print(arr)
-
-newArr = [arr[0]]
-
-
-for i in arr:
-    if i > newArr[-1]:
-        newArr.append(i)
+for case in cases:
+    if lis[-1]<case:
+        lis.append(case)
     else:
-        newArr[bisect_left(newArr, i)] = i
-        print(bisect_left(newArr, i))
+        left = 0
+        right = len(lis)
 
-print(len(newArr))
+        while left<right:
+            mid = (right+left)//2
+            if lis[mid]<case:
+                left = mid+1
+            else:
+                right = mid
+        lis[right] = case
+
+print(lis)
