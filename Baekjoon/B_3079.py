f24e98c8f2b554c6da0d8a5ff35c7288e3dd2f7f
kkm5291 <kkm23125291@gmail.com>
2023-04-04 09:28:07 +0900

    3079 입국심사 : 문제복기 완료 - mid와 total이 이분탐색의 핵심이다

--- Baekjoon/B_3079.py
+++ Baekjoon/B_3079.py
@@ -1,22 +1,25 @@
-import sys
+# total값을 어떻게 잡느냐가 관건임
+# total은 하나당 진행하는 사람의 수?
 
+import sys
 n,m = map(int, sys.stdin.readline().split())
 arr = [int(sys.stdin.readline()) for _ in range(n)]
 
-start = 0
+start = 1
 end = max(arr) * m
 result = 0
 
 while start <= end:
     mid = (start + end) // 2
     total = 0
-    for i in range(n):
-        total += mid // arr[i]
+    
+    for x in arr:
+        total += mid//x
     
     if total >= m:
-        end = mid-1
         result = mid
+        end = mid-1
     else:
-        start = mid + 1
+        start = mid+1
 
 print(result)
