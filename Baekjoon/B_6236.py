8657d8ba21d66fe663bd1669e9f3152b9a91a3d4
kkm5291 <kkm23125291@gmail.com>
2023-04-04 09:57:55 +0900

    6236 용돈관리 : 범위설정이 상당히 헷갈리는데

--- Baekjoon/B_6236.py
+++ Baekjoon/B_6236.py
@@ -1,29 +1,27 @@
-# m번만큼 빼낼 수 있는 k원의 최소값을 구해야 함
-
+# mid와 total을 잘 생각할 것
 import sys
+
 n,m = map(int, sys.stdin.readline().split())
 arr = [int(sys.stdin.readline()) for _ in range(n)]
 
-start = min(arr)
+start = 1
 end = sum(arr)
-
 result = 0
 
 while start <= end:
-    mid = (start+end)//2
+    mid = (start + end) // 2
     money = mid
     total = 1
-    
-    for x in arr:
-        if money - x<0:
-            money = mid
-            total += 1
-        money -= x
 
+    for x in arr:
+        if money - x < 0:
+            total += 1
+            money = mid
+        money -= x
+    
     if total > m or mid < max(arr):
         start = mid + 1
     else:
         end = mid - 1
         result = mid
-
 print(result)
