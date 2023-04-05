aa83d604a30dd8e7be9473aab4a2c8ff7134566d
kkm5291 <kkm23125291@gmail.com>
2023-04-05 09:33:20 +0900

    6236 용돈관리 : end값 설정에도 세심해지자

--- Baekjoon/B_6236.py
+++ Baekjoon/B_6236.py
@@ -1,4 +1,3 @@
-# mid와 total을 잘 생각할 것
 import sys
 
 n,m = map(int, sys.stdin.readline().split())
@@ -7,11 +6,10 @@ arr = [int(sys.stdin.readline()) for _ in range(n)]
 start = 1
 end = sum(arr)
 result = 0
-
 while start <= end:
+    total = 1
     mid = (start + end) // 2
     money = mid
-    total = 1
 
     for x in arr:
         if money - x < 0:
@@ -23,5 +21,5 @@ while start <= end:
         start = mid + 1
     else:
         end = mid - 1
-        result = mid
-print(result)
+
+print(start)
