33d095b7536b321a08fa7858e910316b4163d6a6
kkm5291 <kkm23125291@gmail.com>
2023-04-10 09:50:04 +0900

    1939 중량제한 : 재풀이

--- Baekjoon/B_1939.py
+++ Baekjoon/B_1939.py
@@ -1,28 +1,29 @@
 import sys
 from collections import deque
 
-n,m = map(int, sys.stdin.readline().split())
-bridges = [[] for _ in range(n+1)]
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
-    a,b,c = map(int, sys.stdin.readline().split())
-    bridges[a].append([b,c])
-    bridges[b].append([a,c])
-first,second = map(int, sys.stdin.readline().split())
+    a,b,c=map(int, input().split())
+    graph[a].append([b,c])
+    graph[b].append([a,c])
+first, second = map(int, input().split())
 
 def bfs(weight):
     visited = [False] * (n+1)
     q = deque()
     q.append(first)
     visited[first] = True
-
     while q:
         x = q.popleft()
-        for i,w in bridges[x]:
-            if visited[i] == False and w>=weight:
+        for i,w in graph[x]:
+            if visited[i] == False and weight <= w:
                 visited[i] = True
                 q.append(i)
-    if visited[second]: return True # 방문이 되는 경로임
-    else: return False # 방문이 불가능한 경로임
+    if visited[second] == True: return True
+    else: return False
 
 start = 0
 end = 1000000000
