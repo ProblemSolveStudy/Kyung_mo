976ed72cadbf77fb33eea9ee26a2ca01f96a671b
kkm5291 <kkm23125291@gmail.com>
2023-04-11 10:10:53 +0900

    1561 놀이공원 : 그냥 문제를 못풀었음

--- Baekjoon/B_1561.py
+++ Baekjoon/B_1561.py
@@ -1,41 +1,45 @@
+# mid = 평균적으로 아이들을 전부 태울 수 있는 시간대
+
+# 예제 2번의 경우 6분에 교환됨
+# 아이가 타게 되는 놀이기구의 번호를 알아내야 함
+
+# 시간 T에 놀이기구 R에 탑승한 아이들의 수가 N일 때
+# N = T/R이다?
 import sys
 input = sys.stdin.readline
 
-# 아이들을 모두 태울 값이 mid
-# 아이들 n, 놀이기구 개수 m
+#n 아이들 수 , m 놀이기구 개수
 n,m = map(int, input().split())
-arr = list(map(int, input().split()))
-
-start = 0
-end = max(arr) * m
-result = 0
+time = list(map(int, input().split()))
 
 if n < m:
     print(n)
 else:
     start = 0
-    end = max(arr) * m
+    end = 60000000000
+    result = 0
+
     while start <= end:
-        total = m
         mid = (start + end) // 2
+        total = m
+
+        for t in time:
+            total += mid // t
         
-        for i in arr:
-            total += mid // i
         if total >= n:
-            end = mid - 1
             result = mid
+            end = mid - 1
         else:
             start = mid + 1
-    
-    total = m
+
+
+    cnt = m
     for i in range(m):
-        total += (result - 1) // arr[i]
-    
+        cnt += (result - 1) // time[i]
+
     for i in range(m):
-        if result % arr[i] == 0:
-            total += 1
-        if total == n:
+        if result % time[i] == 0:
+            cnt += 1
+        if cnt == n:
             print(i+1)
             break
-
-    print(result)
