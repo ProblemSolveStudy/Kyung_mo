27b926156fc2b42da50db130d13b82087f41b1cd
kkm5291 <kkm23125291@gmail.com>
2023-04-13 10:25:51 +0900

    2748 피보나치 : 탑다운 문제풀이 추가

--- Baekjoon/B_2748.py
+++ Baekjoon/B_2748.py
@@ -1,12 +1,28 @@
 import sys
 
+# 바텀 업
 n = int(sys.stdin.readline())
 dp = [0] * (n+1)
 
-dp[0] = 0
-dp[1] = 1
+# dp[0] = 0
+# dp[1] = 1
 
-for i in range(2, n+1):
-    dp[i] = dp[i-2] + dp[i-1]
+# for i in range(2, n+1):
+#     dp[i] = dp[i-2] + dp[i-1]
 
-print(dp)
+# print(dp)
+
+
+# 탑 다운
+def fibo(x):
+    if x == 0:
+        return 0
+    if x == 1:
+        return 1
+    if dp[x] != 0:
+        return dp[x]
+
+    dp[x] = fibo(x-1)+fibo(x-2)
+    return dp[x]
+
+print(fibo(n))
