23c01cdaefa24850f4915b31332d7e248296a8ad
kkm5291 <kkm23125291@gmail.com>
2023-04-17 10:54:13 +0900

    11722 가장 긴 감소하는 부분수열 : 이분탐색 풀이법 추가

--- Baekjoon/B_11722.py
+++ Baekjoon/B_11722.py
@@ -1,15 +1,28 @@
 import sys
+# from bisect import bisect_left
+
+# n = int(sys.stdin.readline())
+# arr = list(map(lambda x: int(x) * -1, sys.stdin.readline().split()))
+
+# dp = [arr[0]]
+
+# for i in range(n):
+#     if arr[i] > dp[-1]:
+#         dp.append(arr[i])
+#     else:
+#         idx = bisect_left(dp, arr[i])
+#         dp[idx] = arr[i]
+
+# print(len(dp))
 
 n = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
-dp = [1] * n
 
-for i in range(1,n):
+dp = [1 for _ in range(n)]
+
+for i in range(1, n):
     for j in range(i):
         if arr[j] > arr[i]:
-            dp[i] = max(dp[i], dp[j]+1)
+            dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
-
-# arr을 순회하면서 dp에는 순열 개수를 memorization한다.
-# 
