248ebe175ed779911d13949a8285de8d2c5b03ff
kkm5291 <kkm23125291@gmail.com>
2023-04-24 09:37:08 +0900

    1520 내리막길 : 어렵당

--- Baekjoon/B_1520.py
+++ Baekjoon/B_1520.py
@@ -3,34 +3,34 @@
 # visited = dp라고 생각하면 결국 dfs문제랑 다를게 없음
 # 2번째 줄 조건만 추가하면 될 것 같음
 # x = row y = column
+# 시간초과가 발생하는데?
 
 import sys
-from collections import deque
-
-m,n = map(int, sys.stdin.readline().split())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
-visited = [[0 for _ in range(n)] for _ in range(m)]
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
 def dfs(x,y):
-
     if x == m-1 and y == n-1:
         return 1
-    
-    if visited[x][y] != 0:
-        return visited[x][y]
+
+    if dp[x][y] != -1:
+        return dp[x][y]
 
     cnt = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < m and 0 <= ny < n and graph[x][y] > graph[nx][ny]:
-            cnt += dfs(nx, ny)
-    
-    visited[x][y] = cnt
-    print(visited)
-    return visited[x][y]
+        if 0<=nx<m and 0<=ny<n and arr[nx][ny] < arr[x][y]:
+            cnt += dfs(nx,ny)
+    dp[x][y] = cnt
+    print(dp)
+    return dp[x][y]
 
 
+m,n = map(int, sys.stdin.readline().split())
+arr = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+
+dp = [[-1 for _ in range(n)] for _ in range(m)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 print(dfs(0,0))
