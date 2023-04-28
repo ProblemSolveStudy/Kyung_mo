5e80b8c2550cbab99b65dd91380cd260ea2ffbf0
kkm5291 <kkm23125291@gmail.com>
2023-04-28 16:32:48 +0900

    9456 스티커 : 진짜 내일 당장 다시풀어

--- Baekjoon/B_9456.py
+++ Baekjoon/B_9456.py
@@ -6,7 +6,7 @@ for i in range(int(input())):
     arr = [list(map(int, input().split())) for _ in range(2)]
 
     if n == 1:
-        print(max(arr[0][0], arr[0][1]))
+        print(max(arr[0][0], arr[1][0]))
     else:
         arr[0][1] += arr[1][0]
         arr[1][1] += arr[0][0]
@@ -15,4 +15,3 @@ for i in range(int(input())):
             arr[1][i] += max(arr[0][i-2], arr[0][i-1])
         
         print(max(arr[0][n-1], arr[1][n-1]))
-        
