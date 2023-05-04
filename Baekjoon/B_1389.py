eb3bdf67056820678df1541c81b0b065ecfe3f23
kkm5291 <kkm23125291@gmail.com>
2023-05-04 23:32:35 +0900

    1389 케빈 베이컨의 6단계 법칙 : bfs 풀이

--- Baekjoon/B_1389.py
+++ Baekjoon/B_1389.py
@@ -1,23 +1,27 @@
 import sys
 from collections import deque
+
 n,m = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(n+1)]
+
 for _ in range(m):
     a,b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
 def bfs(start):
+    q = deque([start])
     visited = [0] * (n+1)
-    queue = deque([start])
     visited[start] = 1
-    while queue:
-        target = queue.popleft()
-        for i in graph[target]:
+    while q:
+        s = q.popleft()
+        for i in graph[s]:
             if not visited[i]:
-                visited[i] = visited[target] + 1
-                queue.append(i)
+                q.append(i)
+                visited[i] = visited[s] + 1
+
     return sum(visited)
+
 result = []
 for i in range(1, n+1):
     result.append(bfs(i))
