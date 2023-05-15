caa0e0e4314897f18ab61cd791b5daac64235e07
kkm5291 <kkm23125291@gmail.com>
2023-05-15 10:09:37 +0900

    달리기경주 : enumerate의 존재를 알게됨

---
+++ programmers/P_달리기_경주.py
@@ -0,0 +1,12 @@
+def solution(players, callings):
+    answer = []
+    hashmap = dict()
+    for i,val in enumerate(players):
+        hashmap[val] = i
+    for call in callings:
+        pre, post = hashmap[call] - 1, hashmap[call]
+        hashmap[players[pre]] = post
+        hashmap[players[post]] = pre
+        players[pre], players[post] = players[post], players[pre]
+    answer = players
+    return answer
