9dea6eda0f2e6762bfa5ea39b1c4a469ab79974c
kkm5291 <kkm23125291@gmail.com>
2023-05-22 09:59:20 +0900

    카드 뭉치 : pop(0)을 사용하면 맨 첫요소 반환 가능

--- programmers/P_카드_뭉치.py
+++ programmers/P_카드_뭉치.py
@@ -1,22 +1,13 @@
-from collections import deque
 def solution(cards1, cards2, goal):
-    answer = ''
-    result = []
-    cards1, cards2 = deque(cards1), deque(cards2)
-    for i in range(len(goal)):
-        tmp = goal[i]
-        
-        if tmp in cards1:
-            if cards1[0] == tmp:
-                result.append(cards1.popleft())
-        elif tmp in cards2:
-            if cards2[0] == tmp:
-                result.append(cards2.popleft())
-    
-    if result == goal:
-        answer = "Yes"
-    else:
-        answer = "No"
-    return answer
+
+    for g in goal:
+        if len(cards1) > 0 and g == cards1[0]:
+            cards1.pop(0)
+        elif len(cards2) > 0 and g == cards2[0]:
+            cards2.pop(0)
+        else:
+            return "No"
+
+    return "Yes"
 
 print(solution(["i", "water", "drink"],	["want", "to"],	["i", "want", "to", "drink", "water"]))
