51042b5ab73f9482c142d3449a5e2a181efe1ffa
kkm5291 <kkm23125291@gmail.com>
2023-05-25 09:36:39 +0900

    둘만의 암호 : 반드시 다시풀어볼것

--- programmers/P_둘만의_암호.py
+++ programmers/P_둘만의_암호.py
@@ -1,28 +1,30 @@
-s = "zzzzz"
-skip = "a"
-index = 1
-
+s = "aukks"
+skip = "wbqd"
+index = 5
 def solution(s, skip, index):
-    answer = []
-    s_list = list(s)
-    for word in s_list:
-        new_word = chr(ord(word) + index)
+    answer = ''
+    skip = set(ord(w) for w in skip)
+    for word in s:
+        cnt = index
+        word = ord(word)
+        while cnt != 0:
+            word += 1
+            if word > ord('z'):
+                word = word - ord('z') + ord('a') - 1
+            if word in skip:
+                continue
+            cnt -= 1
+        answer += chr(word)
+    return answer
+    
+    # for i in s:
+    #     i = chr(ord(i) + index)
+    #     if ord(i) + index > ord("z"):
+    #         i = chr(ord(i) + index - 26)
         
-        cnt = 0
-        for i in range(ord(word), ord(new_word) + 1):
-            if chr(i) in skip:
-                cnt += 1
-        
-        new_word = chr(ord(word) + index + cnt)
-        if ord(new_word) >= 123:
-            new_word = chr(ord(new_word) - 26)
-            for i in new_word:
-                if i in skip:
-                    cnt += 1
-            
-        answer.append(chr(ord(new_word) + cnt))
-        
-    for n in answer:
-        print(n, end='')
-
-solution(s, skip, index)
+    #     if i in skip:
+    #         i = chr(ord(i) + 1)
+    #     answer.append(i)
+    
+    # return answer
+print(solution(s, skip, index))
