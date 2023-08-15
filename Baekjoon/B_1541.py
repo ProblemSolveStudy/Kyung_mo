364eefe45e16614e2f8e27b4f6106ba71cfc3ec9
kkm5291 <kkm23125291@gmail.com>
2023-08-15 13:41:31 +0900

    잃어버린 괄호 : 재풀이

--- Baekjoon/B_1541.py
+++ Baekjoon/B_1541.py
@@ -1,28 +1,13 @@
 import sys
+input = sys.stdin.readline().rstrip().split("-")
 
-# arr = sys.stdin.readline().split('-')
-# num = []
-# for i in arr:
-#     sum = 0
-#     s = i.split('+')
-#     for j in s:
-#         sum += int(j)
-    
-#     num.append(sum)
-
-# n = num[0]
-# for i in range(1, len(num)):
-#     n -= num[i]
-
-# print(n)
-
-arr = sys.stdin.readline().split('-')
 num = []
-for i in arr:
+for i in input:
     sum = 0
-    s = i.split('+')
+    s = i.split("+")
     for j in s:
         sum += int(j)
+    
     num.append(sum)
 
 n = num[0]
