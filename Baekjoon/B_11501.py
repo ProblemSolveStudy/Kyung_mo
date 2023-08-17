ebf6544dd5e0af9ffa73d7db5206245823f712e4
kkm5291 <kkm23125291@gmail.com>
2023-08-17 12:44:04 +0900

    주식

---
+++ Baekjoon/B_11501.py
@@ -0,0 +1,14 @@
+import sys
+T = int(sys.stdin.readline())
+stock = list(map(int, sys.stdin.readline().split()))
+
+answer = 0
+max_value = 0
+
+for i in range(len(stock)-1, -1, -1):
+    if stock[i] > max_value:
+        max_value = stock[i]
+    else:
+        answer += max_value - stock[i]
+
+print(answer)
