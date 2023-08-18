54932d6f9d03e05d7970f72b2ba0d9ab0ece94cb
kkm5291 <kkm23125291@gmail.com>
2023-08-18 09:38:52 +0900

    d

--- Baekjoon/B_11000.py
+++ Baekjoon/B_11000.py
@@ -1,45 +1,18 @@
-# import sys
+import sys, heapq
 
-# n = int(sys.stdin.readline())
-# rooms = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-# rooms.sort(key=lambda x: (x[0], x[1]))
+n = int(sys.stdin.readline())
+rooms = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
-# room=[]
-# for start, end in rooms:
-#     if not room:
-#         room.append(end)
-#     else:
-#         allocated = False
-#         for i in range(len(room)):
-#             if room[i] <= start:
-#                 room[i] = end
-#                 allocated = True
-#                 break
-        
-#         if not allocated:
-#             room.append(end)
+answer = []
 
-# print(len(room))
-
-import heapq, sys
-input = sys.stdin.readline
-n = int(input())
-
-q = []
-for i in range(n):
-    start, end = map(int, input().split())
-    q.append([start, end])
-
-q.sort()
-
-room = []
-heapq.heappush(room, q[0][1])
+rooms.sort()
+heapq.heappush(answer, rooms[0][1])
 
 for i in range(1, n):
-    if q[i][0] < room[0]:
-        heapq.heappush(room, q[i][1])
+    if answer[0] > rooms[i][0]:
+        heapq.heappush(answer, rooms[i][1])
     else:
-        heapq.heappop(room)
-        heapq.heappush(room, q[i][1])
+        heapq.heappop(answer)
+        heapq.heappush(answer, rooms[i][1])
 
-print(len(room))
+print(len(answer))
