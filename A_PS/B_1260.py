6ec3094e1d05e760188bd099968f0af92b2ca751
kkm5291 <kkm23125291@gmail.com>
2023-08-20 12:55:29 +0900

    0820

--- A_PS/B_1260.py
+++ A_PS/B_1260.py
@@ -1,20 +1,21 @@
 import sys
 from collections import deque
 
-# n : 정점 m : 간선 v: 탐색 시작 점
 n,m,v = map(int, sys.stdin.readline().split())
-
-graph=[[] for _ in range(n+1)]
-visited=[False] * (n+1)
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
 
 for _ in range(m):
-    a,b = map(int, sys.stdin.readline().rstrip().split())
+    a,b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
+for i in range(len(graph)):
+    graph[i].sort()
+
 def dfs(graph, v, visited):
     visited[v] = True
-    print(v, end = ' ')
+    print(v, end=' ')
     for i in graph[v]:
         if not visited[i]:
             dfs(graph, i, visited)
@@ -24,16 +25,13 @@ def bfs(graph, v, visited):
     visited[v] = True
     while q:
         v = q.popleft()
-        print(v, end= ' ')
+        print(v, end=' ')
         for i in graph[v]:
             if not visited[i]:
                 q.append(i)
                 visited[i] = True
 
-for i in range(len(graph)):
-    graph[i].sort()
-
 dfs(graph, v, visited)
 print()
 visited = [False] * (n+1)
-bfs(graph,v,visited)
+bfs(graph, v, visited)
