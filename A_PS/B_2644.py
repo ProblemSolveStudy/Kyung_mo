737d3bd94d16112692fdcd8c6ded3b91211eb44f
kkm5291 <kkm23125291@gmail.com>
2023-08-29 09:02:21 +0900

    d

--- A_PS/B_2644.py
+++ A_PS/B_2644.py
@@ -1,16 +1,14 @@
 import sys
-n = int(sys.stdin.readline())
-fisrt, second = map(int, sys.stdin.readline().split())
-m = int(sys.stdin.readline())
+input = sys.stdin.readline
 
+n = int(input())
+first, second = map(int, input().split())
+m = int(input())
 graph=[[] for _ in range(n+1)]
-
 for _ in range(m):
-    a,b= map(int,sys.stdin.readline().split())
-
+    a,b = map(int, input().rstrip().split())
     graph[a].append(b)
     graph[b].append(a)
-
 visited = [False] * (n+1)
 result = []
 def dfs(start, cnt, visited):
@@ -21,9 +19,7 @@ def dfs(start, cnt, visited):
     for i in graph[start]:
         if not visited[i]:
             dfs(i, cnt, visited)
-
-dfs(fisrt, 0, visited)
-
+dfs(first, 0, visited)
 if len(result) == 0:
     print(-1)
 else:
