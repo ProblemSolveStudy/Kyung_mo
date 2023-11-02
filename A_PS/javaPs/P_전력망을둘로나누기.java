b8417602f3d8856bad6d7af946286e759416a03b
kkm5291 <kkm23125291@gmail.com>
2023-11-02 15:05:45 +0900

    1102

--- A_PS/javaPs/P_전력망을둘로나누기.java
+++ A_PS/javaPs/P_전력망을둘로나누기.java
@@ -17,17 +17,14 @@ public class P_전력망을둘로나누기 {
         solution.solution(9, wires);
 
     }
-
     class Solution {
+        static ArrayList<Integer>[] graph;
         public int solution(int n, int[][] wires) {
             int answer = -1;
-
-            ArrayList<Integer>[] graph = new ArrayList[n+1];
-
+            graph = new ArrayList[n+1];
             for(int i=0; i<=n; i++) {
                 graph[i] = new ArrayList<>();
             }
-
             for(int[] wire : wires) {
                 int a = wire[0];
                 int b = wire[1];
@@ -35,8 +32,42 @@ public class P_전력망을둘로나누기 {
                 graph[a].add(b);
                 graph[b].add(a);
             }
+            int min = Integer.MAX_VALUE;
 
-            return answer;
+            for(int[] wire: wires) {
+                int a = wire[0];
+                int b = wire[1];
+
+                graph[a].remove(Integer.valueOf(b));
+                graph[b].remove(Integer.valueOf(a));
+
+                min = Math.min(min, Math.abs(bfs(a) - bfs(b)));
+
+                graph[a].add(b);
+                graph[b].add(a);
+            }
+
+            // System.out.println(Arrays.toString(graph));
+            return min;
+        }
+        int bfs(int start) {
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(start);
+
+            boolean[] visited = new boolean[graph.length];
+            int cnt =1;
+            while (!q.isEmpty()) {
+                int s = q.poll();
+                visited[s] = true;
+                List<Integer> temp = graph[s];
+                for(Integer num : temp) {
+                    if (!visited[num]) {
+                        q.offer(num);
+                        cnt++;
+                    }
+                }
+            }
+            return cnt;
         }
     }
 }
