d6a20760daef93dea1cc7980802ca411e3eadd0d
kkm5291 <kkm23125291@gmail.com>
2023-11-29 16:05:43 +0900

    1129

--- A_PS/javaPs/B_2798.java
+++ A_PS/javaPs/B_2798.java
@@ -3,8 +3,6 @@ package javaPs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class B_2798 {
