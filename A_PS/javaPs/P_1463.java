f1f33c9e81a5dbb3a7d22b93c093503a96ce48db
kkm5291 <kkm23125291@gmail.com>
2023-12-20 11:31:35 +0900

    1220

--- A_PS/javaPs/P_1463.java
+++ A_PS/javaPs/P_1463.java
@@ -6,29 +6,25 @@ import java.io.InputStreamReader;
 
 public class P_1463 {
     static int[] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(br.readLine());
-        dp = new int[number + 1];
 
-        System.out.println(calculate(number));
+        int n = Integer.parseInt(br.readLine());
+
+        dp = new int[n+1];
+
+        System.out.println(cal(n));
     }
 
-    static int calculate(int num) {
-        if (num == 1) {
-            return 0;
-        }
-        if (dp[num] > 0) {
-            return dp[num];
-        }
-        dp[num] = calculate(num-1) + 1;
-        if (num % 3 == 0) {
-            dp[num] = Math.min(dp[num], calculate(num / 3) + 1);
-        }
-        if (num % 2 == 0) {
-            dp[num] = Math.min(dp[num], calculate(num / 2) + 1);
-        }
+    static int cal(int n) {
+        if (n==1) return 0;
+        if(dp[n] > 0) return dp[n];
 
-        return dp[num];
+        dp[n] = cal(n-1) + 1;
+        if(n%3 == 0) dp[n] = Math.min(dp[n], cal(n/3) + 1);
+        if(n%2 == 0) dp[n] = Math.min(dp[n], cal(n/2) + 1);
+
+        return dp[n];
     }
 }
