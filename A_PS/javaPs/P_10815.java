8c7437c69f4f144e117feae4b80e0414e7001269
kkm5291 <kkm23125291@gmail.com>
2023-12-28 11:08:53 +0900

    1228

--- A_PS/javaPs/P_10815.java
+++ A_PS/javaPs/P_10815.java
@@ -6,19 +6,19 @@ import java.util.*;
 public class P_10815 {
     static int n;
     static int[] sCard;
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
-
-        sCard = new int[n];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        sCard = new int[n];
         for (int i = 0; i < n; i++) {
             sCard[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(sCard);
+
         int m = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine());
@@ -27,19 +27,20 @@ public class P_10815 {
             int num = Integer.parseInt(st.nextToken());
             sb.append(binarySearch(num) + " ");
         }
+
         System.out.println(sb);
     }
 
     private static int binarySearch(int num) {
-        int left =0;
-        int right = n-1;
+        int start = 0;
+        int end = n-1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+        while(start<=end) {
+            int mid = (start+end) / 2;
             int target = sCard[mid];
 
-            if(num > target) left = mid+1;
-            else if(num < target) right = mid-1;
+            if(num > target) start = mid + 1;
+            else if (num < target) end = mid - 1;
             else return 1;
         }
         return 0;
